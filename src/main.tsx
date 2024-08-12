51293d4fc57f45f1271c6b436cf19ddf67eb1729
Alex <alemumu207@gmail.com>
2024-08-12 19:39:53 +0200

    Agregando el context a nuestra aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// El provider es lo que contiene los datos
+// El context los genera
+import { BudgetProvider } from './context/BudgetContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* Context lo que hace es colocarse sobre la aplicación (el App) */}
+    <BudgetProvider>
+      <App />
+    </BudgetProvider>
   </React.StrictMode>,
 )
