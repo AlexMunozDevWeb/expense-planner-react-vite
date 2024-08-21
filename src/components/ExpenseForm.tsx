452f83b44aa1e4e6763bf5f71c0ee9f9bb3a4055
Alex <alemumu207@gmail.com>
2024-08-21 08:30:00 +0200

    Reiniciar el Formulario y ocultar el modal si se agrega un gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -48,6 +48,13 @@ export default function ExpenseForm() {
     }
     // Agregar un nuevo gasto
     dispatch({type: 'add-expense', payload: { expense }})
+    // Reiniciar el state
+    setExpense({
+      amount: 0,
+      expenseName: '',
+      category: '',
+      date: new Date()
+    })
   }
 
   return (
