4be416ff46f878e5374587aebe805c6f25a15b19
Alex <alemumu207@gmail.com>
2024-08-22 10:53:16 +0200

    Editando el gasto[C

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -53,8 +53,14 @@ export default function ExpenseForm() {
       setError('Todos los campos son obligatorios')
       return
     }
-    // Agregar un nuevo gasto
-    dispatch({type: 'add-expense', payload: { expense }})
+    
+    // Agregar o actualizar el gasto
+    if(state.editingId){
+      dispatch({type: 'update-expense', payload: { expense: {id: state.editingId, ...expense} }})
+    }else{
+      dispatch({type: 'add-expense', payload: { expense }})
+    }
+
     // Reiniciar el state
     setExpense({
       amount: 0,
